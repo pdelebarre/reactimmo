687bcf38357f68ede814262ca9cf91e70641fffd
pdelebarre <pdelebarre@gmail.com>
2022-09-22 10:08:44 +0200

    still mopping...

--- src/components/layout/MUISearchBar.js
+++ src/components/layout/MUISearchBar.js
@@ -3,7 +3,7 @@ import { styled, alpha } from "@mui/material/styles";
 import SearchIcon from "@mui/icons-material/Search";
 
 import InputBase from "@mui/material/InputBase";
-import { Button, IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -21,15 +21,15 @@ const Search = styled("div")(({ theme }) => ({
   },
 }));
 
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
+// const SearchIconWrapper = styled("div")(({ theme }) => ({
+//   padding: theme.spacing(0, 2),
+//   height: "100%",
+//   position: "absolute",
+//   pointerEvents: "none",
+//   display: "flex",
+//   alignItems: "center",
+//   justifyContent: "center",
+// }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
   color: "inherit",
@@ -47,56 +47,56 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 
 const MUISearchBar = ({ setHousesHandler }) => {
   const [location, setLocation] = useState("");
-  const [showSearchButton, setShowSearchButton] = useState(false);
+  // const [showSearchButton, setShowSearchButton] = useState(false);
 
-  function housesHandler(event) {
-    let url = `https://seloger.p.rapidapi.com/locations/search?searchTerm=${location}`;
-    console.log("location", location);
+  // function housesHandler(event) {
+  //   let url = `https://seloger.p.rapidapi.com/locations/search?searchTerm=${location}`;
+  //   console.log("location", location);
 
-    fetch(url, {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "ee888aa438mshf30944eae0cdbcap15616cjsn0f043cc99ba5",
-        "X-RapidAPI-Host": "seloger.p.rapidapi.com",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // console.log("data", data[0].postalCode.replaceAll("|", ","));
+  //   fetch(url, {
+  //     method: "GET",
+  //     headers: {
+  //       "X-RapidAPI-Key": "ee888aa438mshf30944eae0cdbcap15616cjsn0f043cc99ba5",
+  //       "X-RapidAPI-Host": "seloger.p.rapidapi.com",
+  //     },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // console.log("data", data[0].postalCode.replaceAll("|", ","));
 
-        let zipCode = data[0].postalCode.replaceAll("|", ",");
-        console.log("zipCode", zipCode);
+  //       let zipCode = data[0].postalCode.replaceAll("|", ",");
+  //       console.log("zipCode", zipCode);
 
-        let url = `https://seloger.p.rapidapi.com/properties/list?zipCodes=${zipCode}&pageIndex=1&pageSize=2&realtyTypes=2&transactionType=2&sortBy=0&includeNewConstructions=true&maximumFloor=2&maximumPrice=700000&bedrooms=3%2C4%2C5`;
+  //       let url = `https://seloger.p.rapidapi.com/properties/list?zipCodes=${zipCode}&pageIndex=1&pageSize=2&realtyTypes=2&transactionType=2&sortBy=0&includeNewConstructions=true&maximumFloor=2&maximumPrice=700000&bedrooms=3%2C4%2C5`;
 
-        fetch(url, {
-          method: "GET",
-          headers: {
-            "X-RapidAPI-Key":
-              "ee888aa438mshf30944eae0cdbcap15616cjsn0f043cc99ba5",
-            "X-RapidAPI-Host": "seloger.p.rapidapi.com",
-          },
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            let houses = [];
-            console.log("data", data);
-            for (const key in data.items) {
-              //   console.log("...data[key]", ...data[key]);
-              houses.push({ ...data.items[key], id: key });
-            }
-            console.log("houses :>> ", houses);
-            setHousesHandler(houses);
-          });
-      })
+  //       fetch(url, {
+  //         method: "GET",
+  //         headers: {
+  //           "X-RapidAPI-Key":
+  //             "ee888aa438mshf30944eae0cdbcap15616cjsn0f043cc99ba5",
+  //           "X-RapidAPI-Host": "seloger.p.rapidapi.com",
+  //         },
+  //       })
+  //         .then((response) => response.json())
+  //         .then((data) => {
+  //           let houses = [];
+  //           console.log("data", data);
+  //           for (const key in data.items) {
+  //             //   console.log("...data[key]", ...data[key]);
+  //             houses.push({ ...data.items[key], id: key });
+  //           }
+  //           console.log("houses :>> ", houses);
+  //           setHousesHandler(houses);
+  //         });
+  //     })
 
-      .catch((err) => console.error(err));
-  }
+  //     .catch((err) => console.error(err));
+  // }
 
   function changeHandler(event) {
     setLocation(event.target.value);
     console.log("location", location);
-    setShowSearchButton(location.length > 3);
+    // setShowSearchButton(location.length > 3);
   }
 
   function clickHandler(e) {
